c47e14809abcfef85b40b0aa9273566d3bbc1307
Denito37 <Ddrago2002@gmail.com>
2024-05-05 21:19:58 -0400

    refined keyboard controls & reduce code on JS

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const disclaimer = document.querySelector('.disclaimer')
 const intro = document.querySelector('.intro')
 const Q1 = document.getElementById('QU1');
 const Q2 = document.querySelector('.Question_2')
+const result = document.querySelector('.Result')
+const conclusion = document.querySelector('.conclusion')
 
 let Q1_1 = document.getElementsByClassName('adventourous') ;
 let Q1_2 = document.getElementsByClassName('minimalist') ;
@@ -22,30 +24,17 @@ let Q2_2 = document.getElementsByClassName('biking') ;
 let Q2_3 = document.getElementsByClassName('ferry') ;
 let Q2_4 = document.getElementsByClassName('walking') ;
 
-const result = document.querySelector('.Result')
-const conclusion = document.querySelector('.conclusion')
 
-function intro_Q(){
-    intro.scrollIntoView({behavior:"smooth"});
-}
-function scroll_to_Q1(){
-    Q1.scrollIntoView({behavior:"smooth"});
-}
-function scroll_to_Q2(){
-    Q2.scrollIntoView({behavior:"smooth"});
-}
-function scroll_to_Result(){
-    result.scrollIntoView({behavior:"smooth"});
-}
-function scroll_to_Conclusion(){
-    conclusion.scrollIntoView({behavior:"smooth"});
+
+function scroll_to(section){
+    section.scrollIntoView({behavior:"smooth"});
 }
 function retry(){
     Q1.scrollIntoView({behavior:"smooth"});
     answer = []
 }
 
-function myfunction(value) { 
+function myfunction(value) { // checks if section is visible
     const item = value.getBoundingClientRect(); 
     return ( 
         item.top >= 0 && 
@@ -63,30 +52,19 @@ function keyListener(event){
     event = event; //capture the event, and ensure we have an event
     let key = event.key || event.which || event.keyCode; //find the key that was pressed
     if(myfunction(disclaimer) && key == 's'){
-        intro_Q()
+        scroll_to(intro)
     }
-    else if(myfunction(intro) && key == 's'){// intro not visible on scroll
-        scroll_to_Q1()
+    else if(myfunction(intro) && key == 's'){
+        scroll_to(Q1)
     }
     else if(myfunction(result) && key == 's'){
-        scroll_to_Conclusion()
+        scroll_to(conclusion)
     }
-    else if(myfunction(conclusion) && key == 's'){ // conclusion not visible due to footer
+    else if(myfunction(conclusion) && key == 's'){ 
         retry()
     }
 }
-
 window.addEventListener('keydown', keyListener)
-
-window.addEventListener('scroll', () => { 
-    if (myfunction(conclusion)) { 
-        console.log('Element is visible in viewport'); 
-    } else { 
-        console.log('Element is not visible in viewport'); 
-    } 
-}); 
-
-
 
 function adventure(){
     answer =[]
